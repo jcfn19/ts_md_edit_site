965e810034a97b0324863d11fad8b4ca9a732bcf
jcfn19 <jcfnorstrand@outlook.com>
2024-06-03 12:58:27 +0200

    edit btn now works

--- ts_md_umanual_code/src/frontendd.js
+++ ts_md_umanual_code/src/frontendd.js
@@ -11,16 +11,25 @@ console.log("hello world! js");
 // marked.parse('# Marked in the browser\n\nRendered by **marked**.');
 
 async function bgetusers() {
-    const response = await fetch("/usersrawjson");
-    const data = await response.json();
-    console.log(data);
+  const response = await fetch("/usersrawjson");
+  const data = await response.json();
+  console.log(data);
 
-    let testuser = data[0].uname;
-    console.log(testuser);
-    // hvis user som er logget inn sinn rolle er admin, tillat å edite siden
+  let testuser = data[0].uname;
+  console.log(testuser);
+  // hvis user som er logget inn sinn rolle er admin, tillat å edite siden
 }
 
 bgetusers()
+
+function editf(){
+  var x = document.getElementById("editcontents");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
 function updateIframe() {
   var text = document.getElementById('text field').value;
@@ -29,4 +38,9 @@ function updateIframe() {
   iframe.contentWindow.document.open();
   iframe.contentWindow.document.write(marked.parse(text));
   iframe.contentWindow.document.close();
+}
+
+function savechangesf(){
+  var text = document.getElementById('text field').value;
+  document.getElementById('contents').innerHTML = marked.parse(text);
 }
