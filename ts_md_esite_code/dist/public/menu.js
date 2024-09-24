217cc440875119ded5cf4dc869b4abf77a855f87
jcfn19 <jcfnorstrand@outlook.com>
2024-09-24 09:40:26 +0200

    Updated menu, menu btns don't work yet.

--- ts_md_esite_code/dist/public/menu.js
+++ ts_md_esite_code/dist/public/menu.js
@@ -1,3 +1,4 @@
+//function for opening/closing the menu
 function openNavf() {
     const sidenav = document.getElementById("idSidenav");
     sidenav.style.width = "250px";
@@ -10,5 +11,29 @@ function closeNavf() {
 }
 const closenavbutton = document.getElementById('closeNav');
 closenavbutton.onclick = closeNavf;
+//function for editing the files in the menu
+async function editmenuf() {
+    const response = await fetch("/userroleraw");
+    const data = await response.json();
+    console.log(data);
+    if (data == "admin") {
+        var x = document.getElementById("sidenavbtns");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        }
+        else {
+            x.style.display = "none";
+        }
+        const y = document.querySelectorAll('input[type="checkbox"]');
+        y.forEach(y => {
+            y.style.display = y.style.display === 'none' ? 'inline-block' : 'none';
+        });
+    }
+    else {
+        return console.log("Invalid role: " + data);
+    }
+}
+const editbutton = document.getElementById('editmenubtn');
+editbutton.onclick = editmenuf;
 export {};
 //# sourceMappingURL=menu.js.map
--- ts_md_esite_code/src/public/menu.ts
+++ ts_md_esite_code/src/public/menu.ts
@@ -1,3 +1,4 @@
+//function for opening/closing the menu
 function openNavf(): void {
     const sidenav = document.getElementById("idSidenav") as HTMLElement;
     sidenav.style.width = "250px";
@@ -13,3 +14,28 @@ function closeNavf(): void {
 
 const closenavbutton = document.getElementById('closeNav') as HTMLButtonElement; 
 closenavbutton.onclick = closeNavf;
+
+//function for displaying/hiding the upload and delete btns & checkboxes in the menu
+async function editmenuf(){
+    const response = await fetch("/userroleraw");
+    const data = await response.json();
+    console.log(data);
+
+    if (data == "admin") {
+        var x = document.getElementById("sidenavbtns");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+        const y = document.querySelectorAll<HTMLInputElement>('input[type="checkbox"]');
+        y.forEach(y => {
+            y.style.display = y.style.display === 'none' ? 'inline-block' : 'none';
+        });
+    } else {
+        return console.log("Invalid role: " + data);
+    }
+}
+
+const editbutton = document.getElementById('editmenubtn') as HTMLButtonElement; 
+editbutton.onclick = editmenuf;
